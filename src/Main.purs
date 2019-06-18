205b7b328b30da36f7edebe83fb4d3bf8f84b1fb
TAHARA Yoshinori <read.eval.print@gmail.com>
2019-06-18 17:53:37 +0900

    pushState

--- src/Main.purs
+++ src/Main.purs
@@ -9,38 +9,41 @@ import Control.Coroutine.Aff (emit)
 import Control.Coroutine.Aff as CoroutineAff
 import Data.Foldable (traverse_)
 import Data.Maybe (Maybe(..))
-import Data.String.CodeUnits as Str
 import Effect (Effect)
 import Effect.Aff (Aff)
 import Effect.Class (liftEffect)
+import Effect.Console (log)
 import Halogen as H
 import Halogen.Aff as HA
 import Halogen.VDom.Driver (runUI)
 import Web.Event.EventTarget (addEventListener, eventListener)
 import Web.HTML (window)
-import Web.HTML.Event.HashChangeEvent as HCE
-import Web.HTML.Event.HashChangeEvent.EventTypes as HCET
+import Web.HTML.Event.PopStateEvent as HCE
+import Web.HTML.Event.PopStateEvent.EventTypes as HCET
+import Web.HTML.Location (href)
+import Web.HTML.Window (location)
 import Web.HTML.Window as Window
 
 -- A producer coroutine that emits messages whenever the window emits a
 -- `hashchange` event.
-hashChangeProducer :: Coroutine.Producer HCE.HashChangeEvent Aff Unit
-hashChangeProducer = CoroutineAff.produce \emitter -> do
+popStateProducer :: Coroutine.Producer HCE.PopStateEvent Aff Unit
+popStateProducer = CoroutineAff.produce \emitter -> do
   listener <- eventListener (traverse_ (emit emitter) <<< HCE.fromEvent)
   liftEffect $
     window
       >>= Window.toEventTarget
-      >>> addEventListener HCET.hashchange listener false
+      >>> addEventListener HCET.popstate listener false
 
 -- A consumer coroutine that takes the `query` function from our component IO
 -- record and sends `ChangeRoute` queries in when it receives inputs from the
 -- producer.
-hashChangeConsumer
+popStateConsumer
   :: (forall a. Routing.Query a -> Aff (Maybe a))
-  -> Coroutine.Consumer HCE.HashChangeEvent Aff Unit
-hashChangeConsumer query = CR.consumer \event -> do
-  let hash = Str.drop 1 $ Str.dropWhile (_ /= '#') $ HCE.newURL event
-  void $ query $ H.tell $ Routing.ChangeRoute hash
+  -> Coroutine.Consumer HCE.PopStateEvent Aff Unit
+popStateConsumer query = CR.consumer \event -> do
+  path <- liftEffect $ window >>= location >>= href
+  H.liftEffect $ log path
+  void $ query $ H.tell $ Routing.ChangeRoute path
   pure Nothing
 
 main :: Effect Unit
@@ -50,4 +53,4 @@ main = HA.runHalogenAff do
   -- Connecting the consumer to the producer initializes both, adding the event
   -- listener to the window and feeding queries back to our component as events
   -- are received.
-  Coroutine.runProcess (hashChangeProducer Coroutine.$$ hashChangeConsumer io.query)
+  Coroutine.runProcess (popStateProducer Coroutine.$$ popStateConsumer io.query)
