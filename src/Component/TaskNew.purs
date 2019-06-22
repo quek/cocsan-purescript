40626e862cd47926bec8a52632e0a1d0f06803fc
TAHARA Yoshinori <read.eval.print@gmail.com>
2019-06-22 14:26:38 +0900

    formless

--- src/Component/TaskNew.purs
+++ src/Component/TaskNew.purs
@@ -2,17 +2,51 @@ module Coc.Component.TaskNew where
 
 import Prelude
 
+import Data.Either (Either(..))
+import Data.Maybe (Maybe(..))
+import Data.Newtype (class Newtype)
+import Data.Symbol (SProxy(..))
 import Effect.Aff (Aff)
+import Effect.Console (log)
+import Formless as F
 import Halogen as H
 import Halogen.HTML as HH
+import Halogen.HTML.Events as HE
+import Halogen.HTML.Properties as HP
 
+-----------------------------------------------------------------------------
+-- form
+type TaskInput = { name :: String }
+
+data NameError = Required
+
+newtype TaskForm r f = TaskForm (
+  r
+  --          error     input  output
+  ( name :: f NameError String String )
+  )
+
+derive instance nettypeTaskForm :: Newtype (TaskForm r f) _
+
+input :: forall m. Monad m => F.Input' TaskForm m
+input =
+  { initialInputs: Nothing      -- same as: Just (F.wrapInputFields { name: "" })
+  , validators: TaskForm
+      { name: F.hoistFnE_ \str -> case str of
+           "" -> Left Required
+           _ -> Right str
+      }
+  }
+
+-----------------------------------------------------------------------------
+-- component
 type Slot = H.Slot Query Void
 
 data Query a = Dummy a
 
 type State = {}
 
-data Action = Foo
+data Action = HandleSubmit TaskInput
 
 component :: forall i message q. H.Component HH.HTML q i message Aff
 component =
@@ -21,17 +55,37 @@ component =
     , render
     , eval: H.mkEval $ H.defaultEval { handleAction = handleAction }
     }
+  where
+  initialState :: i -> State
+  initialState _ = {}
 
-initialState :: forall i. i -> State
-initialState _ = {}
+  render state =
+    HH.div_
+      [ HH.p_ [ HH.text "新規だよ" ]
+      , HH.slot F._formless unit formComponent unit (Just <<< HandleSubmit)
+      ]
 
-render :: forall action m. State -> H.ComponentHTML action () m
-render state =
-  HH.div_
-    [ HH.p_ [ HH.text "新規だよ" ]
-    ]
+  handleAction = case _ of
+    HandleSubmit task -> do
+      H.liftEffect $ log $ show task
+      pure unit
 
-handleAction :: forall message. Action → H.HalogenM State Action () message Aff Unit
-handleAction = case _ of
-  Foo ->
-    pure unit
+  formComponent = F.component (const input) $ F.defaultSpec { render = renderForm, handleEvent = F.raiseResult }
+    where
+    renderForm st@{ form } =
+      HH.div_
+        [ HH.input
+            [ HP.value $ F.getInput _name form
+            , HP.placeholder "内容"
+            , HE.onValueInput $ Just <<< F.set _name
+            ]
+        , HH.text case F.getError _name form of
+            Nothing -> ""
+            Just Required -> "必須です。"
+        , HH.button
+            [ HE.onClick \_ -> Just F.submit ]
+            [ HH.text "OK" ]
+        ]
+      where
+      _name = SProxy :: SProxy "name"
+
