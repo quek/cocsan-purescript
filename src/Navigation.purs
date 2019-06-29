3ecec44cc5d45275235c1dd6cf5d4d35a126a13d
TAHARA Yoshinori <read.eval.print@gmail.com>
2019-06-29 12:57:47 +0900

    .

--- src/Navigation.purs
+++ src/Navigation.purs
@@ -2,12 +2,16 @@ module Coc.Navigation where
 
 import Prelude
 
+import Effect.Class (class MonadEffect)
 import Foreign (unsafeToForeign)
 import Halogen as H
+import Halogen.Query.HalogenM (HalogenM)
 import Routing.PushState (makeInterface)
 
 data Message = UrlChanged String
 
+go :: forall m slots action state.
+      MonadEffect m => String -> HalogenM state action slots Message m Unit
 go path = do
   H.liftEffect do
     nav <- makeInterface
